4a63bd8c5f94f23a1915322efb36ce350dfe3247
LuisAlves18 <luisp.gama.alves@gmail.com>
2022-01-17 15:51:33 +0000

    structure

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const express = require("express");
-const port = 3000;
+const express = require('express')
+const mongoose = require('mongoose');
 const app = express();
-const wget = require("node-wget")
-app.get("/", function (req, res) {
+const port = 3000;
+const wget = require("node-wget");
+app.use(express.json());
 
-wget({url: "http://portfir.insa.pt/assets/downloads/insa_tca.xlsx", dest: "./dbPortfir.xlsx"}, function(){
-    var XLSX = require("xlsx");
-  var workbook = XLSX.readFile("./dbPortfir.xlsx");
-  var sheet_name_list = workbook.SheetNames;
-  var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-  console.log(xlData);
-  res.send(xlData);
+app.get("/", function (req, res) {
+    wget(
+      {
+        url: "http://portfir.insa.pt/assets/downloads/insa_tca.xlsx",
+        dest: "./dbPortfir.xlsx",
+      },
+      function () {
+        var XLSX = require("xlsx");
+        var workbook = XLSX.readFile("./dbPortfir.xlsx");
+        var sheet_name_list = workbook.SheetNames;
+        var xlData = XLSX.utils.sheet_to_json(
+          workbook.Sheets[sheet_name_list[0]]
+        );
+        console.log(xlData);
+        res.send(xlData);
+      }
 });
- 
-  
+// MONGOOSE
+mongoose.connect('mongodb://mgrocery:muf8JKQmyw7zDCYHym0PWGh3O8EKegccH@35.242.162.250:27017/mGrocery', { useNewUrlParser: true, useUnifiedTopology: true });
+// TSIW
+//mongoose.connect('mongodb+srv://tsiw:GAa8xvmV3eKrVa8C@cluster0.b0vmz.mongodb.net/TSIW?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("connected to MongoDB")
 });
-app.listen(port, function () {
-  console.log("listening at port", port);
-});
+app.listen(port, function() {
+    console.log(`Server running at http://localhost:${port}`);
+})
---
+++ index1.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const port = 3000;
+const app = express();
+const wget = require("node-wget");
+
+app.use(express.json());
+app.get("/", function (req, res) {
+  wget(
+    {
+      url: "http://portfir.insa.pt/assets/downloads/insa_tca.xlsx",
+      dest: "./dbPortfir.xlsx",
+    },
+    function () {
+      var XLSX = require("xlsx");
+      var workbook = XLSX.readFile("./dbPortfir.xlsx");
+      var sheet_name_list = workbook.SheetNames;
+      var xlData = XLSX.utils.sheet_to_json(
+        workbook.Sheets[sheet_name_list[0]]
+      );
+      console.log(xlData);
+      res.send(xlData);
+    }
+  );
+  mongoose.connect(
+    "mongodb://mgrocery:muf8JKQmyw7zDCYHym0PWGh3O8EKegccH@35.242.162.250:27017/mGrocery",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("connected to MongoDB");
+  });
+});
+app.listen(port, function () {
+  console.log("listening at port", port);
+});
