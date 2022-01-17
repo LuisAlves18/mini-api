44286e5ead033fb954422c046d149975b4e35d0d
LuisAlves18 <luisp.gama.alves@gmail.com>
2022-01-17 23:03:02 +0000

    last commit resolved

---
+++ models/product_model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const nutritionalSchema = mongoose.Schema({
+    nome: String,
+    energia: Number,
+    lipidos: Number,
+    hidratos: Number,
+    acucares: Number,
+    fibra: Number,
+    proteina: Number,
+    sal: Number
+});
+
+const product = mongoose.model('iopaymentcard_portfir', nutritionalSchema);
+
+module.exports = product; 
